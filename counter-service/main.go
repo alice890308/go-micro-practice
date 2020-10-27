d65b346655e7edfa7b7f34628f0d7e1b92137ac6
唐晏湄 <alice@tangyanmeideMacBook-Pro.local>
2020-10-27 23:21:45 +0800

    ll

--- counter-service/main.go
+++ counter-service/main.go
@@ -14,7 +14,7 @@ type CounterService struct {}
 var number int = 1
 
 // Count ... Implement interface left in proto/counter.pb.micro.go server part
-func (g *CounterService) Count(ctx context.Context, req *counter.Request, res *counter.Response) error {
+func (g *CounterService) Counter(ctx context.Context, req *counter.Request, res *counter.Response) error {
   log.Println("Counter service handle Count")
   res.Number = strconv.Itoa(number)
   number++
